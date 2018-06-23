1c29641f28b5e4140bbe2ccad7630598ea99cdc9
Igorko <55022@mail.ru>
2018-06-23 20:35:16 +0300

    Rewrote stuff in php

---
+++ kek_kek_project/basic/views/normal_user/list.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ */
+?>
+
+<html>
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Project CSS -->
+	<link href="css/project.css" rel="stylesheet">
+	<!-- Jquery and bootstrap scripts -->
+	<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/page_transfer.js"></script>
+    
+	<head>
+		<title>
+			Список замовлень
+		</title>
+	</head>
+
+	<body>
+		<div class="head_bar btn-group">
+			<button type="button" class="btn btn-default bar_button col-md" onclick="goToPage('profile.php')">Профіль</button>
+            <button type="button" class="btn btn-default bar_button col-md" onclick="goToPage('list.php')">Бронювання</button>
+            <button type="button" class="btn btn-default bar_button col-md" onclick="goToPage('settings.php')">Налаштування</button>
+            <button type="button" class="btn btn-default bar_button col-md" onclick="">Вийти</button>
+		</div>
+		<div class="page_container">
+			<!-- Page contents -->
+			<div class="block_container">
+					<h1>Відкрито доступ до усіх сторінок!</h1>
+					<p>Проте не всі працюють :P</p>
+				</div>
+				<hr/>
+				<div class="block_container">
+					<h1>Нова сторінка - предмети</h1>
+					<p>Тепер ви можете преглядати домашні завдання та матеріали по предметах у відповідному розділі.</p>
+				</div>
+				<hr/>
+				<div class="block_container">
+					<h1>Сторінку новин створено!</h1>
+					<p>Перший запис.</p>
+				</div>
+				<hr/>
+		</div>
+	</body>
+</html>
